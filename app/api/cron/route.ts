7945b4f5fb9c0f396e7167674b44ff5ff0d4b8a4
shrutib11 <shrutibhalodia11@gmail.com>
2024-05-29 10:46:09 +0530

    first commit

---
+++ app/api/cron/route.ts
@@ -0,0 +1,75 @@
+import Product from "@/lib/models/product.model"
+import { connectToDB } from "@/lib/mongoose"
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer"
+import { scrapeAmazonProduct } from "@/lib/scraper"
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils"
+import { NextResponse } from "next/server"
+
+//Send periodically email when price updates
+export async function GET(){
+    try {
+        connectToDB()
+
+        const products = await Product.find({})
+
+        if(!products) throw new Error("No products found")
+
+        //Scrape latest product details and update DB
+        const updatedProducts = await Promise.all(
+            products.map(async (currentProduct) => {
+                const scrapedProduct = await scrapeAmazonProduct(currentProduct.url)
+
+                if(!scrapedProduct) return
+
+                    const updatedPriceHistory = [
+                        ...currentProduct.priceHistory,
+                        {
+                          price: scrapedProduct.currentPrice,
+                        },
+                      ];
+              
+                      const product = {
+                        ...scrapedProduct,
+                        priceHistory: updatedPriceHistory,
+                        lowestPrice: getLowestPrice(updatedPriceHistory),
+                        highestPrice: getHighestPrice(updatedPriceHistory),
+                        averagePrice: getAveragePrice(updatedPriceHistory),
+                      };
+              
+                      // Update Products in DB
+                      const updatedProduct = await Product.findOneAndUpdate(
+                        {
+                          url: product.url,
+                        },
+                        product
+                      );
+
+                      // Check Each Product's status & send email accordingly
+                      const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
+
+                      if(emailNotifType && updatedProduct.users.length > 0){
+                        const productInfo = {
+                            title : updatedProduct.url,
+                            url : updatedProduct.title
+                        }
+
+                        const emailContent = await generateEmailBody(productInfo, emailNotifType)
+
+                        //find all users who subscribes to current product and send email to all
+                        const userEmails = updatedProduct.users.map((user:any) => user.email)
+
+                        await sendEmail(emailContent, userEmails)
+                      }
+
+                      return updatedProduct
+
+            })
+        )
+
+        return NextResponse.json({
+            message : 'OK', data : updatedProducts
+        })
+    } catch (error) {
+        throw new Error(`Error in GET : ${error}`)
+    }
+}
---
+++ lib/scraper/index.ts
@@ -0,0 +1,95 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice, extractReviewsCount, extractStarRating } from "../utils";
+
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+
+  //curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_0364c5b3-zone-pricewise:ip1qk1sg0ifc -k "http://lumtest.com/myip.json"
+
+  //Brightdata proxy confifuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+
+  try {
+    //fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data); // initialize cheerio for the first name so name it as $
+
+    //Extract the product title
+    const title = $("#productTitle").text().trim();
+
+    //At this different locations, price element can be found
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+    const images = 
+      $('#imgBlkFront').attr('data-a-dynamic-image') || 
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+      const imageUrls = Object.keys(JSON.parse(images));
+
+      const currency = extractCurrency($('.a-price-symbol'))
+      const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");         //remove everything that is not %
+      
+      const stars = extractStarRating(
+        $('#averageCustomerReviews')
+      )
+
+      const description = extractDescription($)
+
+      const reviewsCount = extractReviewsCount(
+        $("#acrCustomerReviewLink")
+      )
+
+      // Construct data object with scraped information
+    const data = {
+        url,
+        currency: currency || '$',
+        image: imageUrls[0],
+        title,
+        currentPrice: Number(currentPrice) || Number(originalPrice),
+        originalPrice: Number(originalPrice) || Number(currentPrice),
+        priceHistory: [],
+        discountRate: Number(discountRate),
+        category: 'category',       //default
+        reviewsCount:reviewsCount || 0,           //default
+        stars: stars || 0,                 
+        isOutOfStock: outOfStock,
+        description,
+        lowestPrice: Number(currentPrice) || Number(originalPrice),
+        highestPrice: Number(originalPrice) || Number(currentPrice),
+        averagePrice: Number(currentPrice) || Number(originalPrice),
+      }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(`Failed to scrape product : ${error.message}`);
+  }
+}
